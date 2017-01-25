f6e14c7999cd82b0e38f26b98fd5a46755c3f35f
JunYeongJang <wnsdud3256@gmail.com>
2017-01-26 00:03:50 +0900

    requirements.txt

--- hashboard/settings.py
+++ hashboard/settings.py
@@ -37,9 +37,14 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'rest_framework',
+    'rest_framework.authtoken',
+
     'api',
     'accounts.apps.AccountsConfig',
     'chargers.apps.ChargersConfig',
+
 )
 
 MIDDLEWARE_CLASSES = (
