4444588f0a4189da18a5d3f448251a253b23b60e
JunYeongJang <wnsdud3256@gmail.com>
2017-01-26 13:01:52 +0900

    createboards command create

---
+++ boards/management/commands/createboards.py
@@ -0,0 +1,40 @@
+
+from django.core.management.base import BaseCommand, CommandError
+
+import requests
+from bs4 import BeautifulSoup
+
+from boards.models import Board
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+        board_url = "http://ev.or.kr/portal/board/8/"
+        for x in range(1, 1500):
+            board_id = str(x)
+            board_req = board_url + board_id
+            r = requests.get(board_req)
+            if r.text.count('충전인프라 정보시스템 에러 내용'):
+                pass
+            else:
+                soup = BeautifulSoup(r.text, 'html.parser')
+
+                for data in soup.find_all('em'):
+                    data_str = str(data.get_text())
+                    if data_str.count('-'):
+                        board_date = data_str
+
+                    elif data_str.isdigit():
+                        board_count = data_str
+                    else:
+                        board_writer = data_str
+
+                board_cont = soup.find("div", {"class": "view_cont"})
+                board_cont = str(board_cont.get_text())
+                board_title = soup.find("h3")
+                board_title = str(board_title.get_text())
+                try:
+                    hash_board = Board.objects.create(title=board_title, writer=board_writer, content=board_cont, date=board_date)
+                except:
+                    return
--- boards/models.py
+++ boards/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Board(models.Model):
     date = models.CharField(max_length=20)
-    title = models.TextField()
+    title = models.CharField(max_length=200,unique=True)
     writer = models.CharField(max_length=20)
     content = models.TextField()
 
