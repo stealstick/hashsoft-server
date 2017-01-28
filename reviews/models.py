2acd15dc0f1d348e2fd81bb43691b5c96800addf
JunYeongJang <wnsdud3256@gmail.com>
2017-01-28 17:22:10 +0900

    Reviews App model 수정 및 Test Case 수정

--- reviews/models.py
+++ reviews/models.py
@@ -1,14 +1,15 @@
 from django.db import models
 
 from accounts.models import User
-# Create your models here.
+from chargers.models import Charger
 
-class Review(models.Model):
-    review_number = models.AutoField(primary_key=True, default=1)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+class ChargerReview(models.Model):
+    charger = models.ForeignKey(Charger, on_delete=models.CASCADE, related_name="review")
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="charger_review")
     title = models.TextField()
     text = models.TextField()
     star = models.PositiveIntegerField(default=1)
 
     def __str__(self):
         return self.title
+
--- reviews/tests.py
+++ reviews/tests.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
 
 from accounts.models import User
-from .models import Review
+from chargers.models import Charger
 
 # Create your tests here.
 
@@ -11,16 +11,17 @@ class ReviewTestCase(TestCase):
             username='guest1', email='guest1@gmail.com', password='hashsoft3455',
             year=22, sex="남자", place="서울특별시"
         )
-        self.guest2 = User.objects.create_user(
-            username='guest2', email='guest2@gmail.com', password='hashsoft3455',
-            year=24, sex="남자", place="서울특별시"
+        self.charger1 = Charger.objects.create(
+            statId=11110003, statNm="종묘 공영주차장", chgerId=1, chgerType=3,
+            stat=2, addrDoro="경상남도 창원시 마산합포구 구산면 구산로 554", lat=35.119457244873,
+            lng=128.580612182617, useTime="24시간 이용가능", LastUsedTime="nodata"
         )
-        self.guest1.review_set.create(
+        self.guest1.charger_review.create(
             title="좋아요!", text="충전도 빠르고 서비스도 주셔서 너무 좋았어요~",
-            star=5
+            star=5, charger=self.charger1
             )
 
     def test_user_reviews_check(self):
-        guest1_review1 = self.guest1.review_set.all()[0]
+        guest1_review1 = self.guest1.charger_review.all()[0]
         self.assertEqual(guest1_review1.title, "좋아요!")
         self.assertEqual(guest1_review1.text, "충전도 빠르고 서비스도 주셔서 너무 좋았어요~")
