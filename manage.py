c24d7616c4489ee6f9dd3aa995ebb26c2c7833d6
JunYeongJang <wnsdud3256@gmail.com>
2017-01-28 13:14:49 +0900

    setting file split

--- manage.py
+++ manage.py
@@ -1,9 +1,15 @@
 #!/usr/bin/env python
 import os
 import sys
+from getpass import getuser
+
+
+def set_local_setting():
+    username = getuser()
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', "hashboard.settings.local_{}".format(username))
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hashboard.settings")
+    set_local_setting()
 
     from django.core.management import execute_from_command_line
 
