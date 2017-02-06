a1e2fc08e8d3c1ebca92687480543c91ab296d8c
JunYeongJang <wnsdud3256@gmail.com>
2017-02-06 12:11:44 +0900

    user serializer hotfix

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -4,4 +4,6 @@ from .models import User
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('pk', 'username',  'profile', 'sex', 'sex', 'place')
+        fields = ('pk', 'username', 'email', 'year', 'sex', 'place', 'profile', 'password')
+        extra_kwargs = {'password': {'write_only': True}
+                        }
