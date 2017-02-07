cc819a56ff3bb54ac2f529c11780627854bdaea6
JunYeongJang <wnsdud3256@gmail.com>
2017-02-07 16:46:30 +0900

    Git 오류 수정

--- reviews/views.py
+++ reviews/views.py
@@ -2,6 +2,7 @@ from rest_framework import viewsets
 from .models import ChargerReview
 from .serializers import ChargerReviewSerializer
 
+
 class ChargerReviewViewSet(viewsets.ModelViewSet):
         serializer_class = ChargerReviewSerializer
         queryset = ChargerReview.objects.all()
