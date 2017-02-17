df8003c9821e558464686c2fa4f773ebf38862fd
JunYeongJang <wnsdud3256@gmail.com>
2017-02-17 23:01:03 +0900

    User Model Sex choice

--- accounts/models.py
+++ accounts/models.py
@@ -3,6 +3,7 @@ import time
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, UserManager
 from django.db import models
 
+
 def get_upload_path(instance, filename):
     currently = time.strftime("%y%m%d%H%M%S")
     return os.path.join(
@@ -11,6 +12,10 @@ def get_upload_path(instance, filename):
 
 
 class User(AbstractBaseUser, PermissionsMixin):
+    SEX_CHOICES = (
+        ("남자", "남자"),
+        ("여자", "여자"),
+    )
     objects = UserManager()
     email = models.CharField(max_length = 40, unique=True)
     username = models.CharField(max_length=50)
@@ -18,7 +23,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     is_staff = models.BooleanField(default=True)
     date_joined =models.BooleanField(default=True)
     year = models.PositiveIntegerField(default=0, help_text="year")
-    sex = models.CharField(max_length=10, help_text="성")
+    sex = models.CharField(max_length=10, help_text="성별", choices=SEX_CHOICES)
     car_type = models.CharField(max_length=300, help_text="차 종류")
     place = models.CharField(max_length=100, help_text="거주지")
     profile = models.ImageField(upload_to=get_upload_path, default="defalutProfileImg.jpg")
