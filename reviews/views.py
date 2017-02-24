4029c5e0752d7ba0170d0b035ce7584541d8dc84
JunYeongJang <wnsdud3256@gmail.com>
2017-02-24 22:55:43 +0900

    statId로 추가 가능하게

--- reviews/views.py
+++ reviews/views.py
@@ -1,12 +1,11 @@
 from rest_framework import viewsets
-from rest_framework.response import Response
-from rest_framework.decorators import api_view
 from .serializers import ChargerReviewSerializer
-from chargers.models import Charger
 from .models import ChargerReview
-from rest_framework import status
 
 
 class ChargerReviewViewSet(viewsets.ModelViewSet):
         serializer_class = ChargerReviewSerializer
         queryset = ChargerReview.objects.all()
+
+        def perform_create(self, serializer):
+                serializer.save(user=self.request.user)
